27e66b528f12ca6acfcb5a2e492f243f81044b66
Steven <Steven@Stevens-iMac.local>
2020-03-15 15:37:46 +0000

    changed test

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -10,7 +10,7 @@ describe 'fizzbuzz' do
   it 'returns "buzz" when passed 5' do
     expect(fizzbuzz(5)).to eq 'buzz'
   end
-
+end
   describe 'fizzbuzz' do
     it 'returns "buzz" when passed 5' do
       expect(fizzbuzz(6)).to eq 'fizz'
